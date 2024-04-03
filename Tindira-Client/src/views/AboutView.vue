de42440207c0740d4556e8474669c7eedea7ec13
Ben-avrahami <ben.avrahami@intel.com>
2024-04-03 14:55:03 +0300

    Add Prime Vue+ initial pinia store

---
+++ Tindira-Client/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <p class="text-3xl font-bold underline text-orange-600">
+      Hello world!!!!!!@
+    </p>
+    <iconify-icon icon="ph:heart-fill"></iconify-icon>
+  </div>
+
+
+
+</template>
+<script setup lang="ts">
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ Tindira-Client/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <Knob v-model="value" />
+    <div>increment is {{ userStore.increment }}</div>
+    <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City"
+      class="w-full md:w-[14rem]" />
+  </main>
+</template>
+
+<script setup lang="ts">
+
+import { useAppStore } from '../stores/app';
+import { ref } from "vue";
+const userStore = useAppStore();
+import Knob from 'primevue/knob';
+import Dropdown from 'primevue/dropdown';
+let value = 0;
+
+const selectedCity = ref();
+const cities = ref([
+  { name: 'New York', code: 'NY' },
+  { name: 'Rome', code: 'RM' },
+  { name: 'London', code: 'LDN' },
+  { name: 'Istanbul', code: 'IST' },
+  { name: 'Paris', code: 'PRS' }
+]);
+userStore.AddIncrement();
+</script>
