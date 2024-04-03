9ad00af186716d54a11fbc06406855cdc97f1530
Ben-avrahami <ben.avrahami@intel.com>
2024-04-03 18:50:29 +0300

    remove unneeded files + add dark mode toggle

--- Tindira-Client/src/views/AboutView.vue
+++ Tindira-Client/src/views/AboutView.vue
@@ -3,6 +3,10 @@
     <p class="text-3xl font-bold underline text-orange-600">
       Hello world!!!!!!@
     </p>
+    <Button @click="console.log('clicked')">
+      <iconify-icon icon="mdi:home" class="mr-4"></iconify-icon>
+      <span class="animate-bounce">test if dark mode persists</span>
+    </Button>
     <iconify-icon icon="ph:heart-fill"></iconify-icon>
   </div>
 
@@ -10,6 +14,7 @@
 
 </template>
 <script setup lang="ts">
+import Button from 'primevue/Button';
 </script>
 <style>
 @media (min-width: 1024px) {
--- Tindira-Client/src/views/HomeView.vue
+++ Tindira-Client/src/views/HomeView.vue
@@ -4,9 +4,9 @@
     <div>increment is {{ userStore.increment }}</div>
     <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City"
       class="w-full md:w-[14rem]" />
-    <Button @click="console.log('clicked')">
+    <Button @click="toggleDarkMode">
       <iconify-icon icon="mdi:home" class="mr-4"></iconify-icon>
-      <span class="animate-bounce">Sample Btn! with icon</span>
+      <span class="animate-bounce">Dark Mode Toggle</span>
     </Button>
   </main>
 </template>
@@ -30,4 +30,14 @@ const cities = ref([
   { name: 'Paris', code: 'PRS' }
 ]);
 userStore.AddIncrement();
+
+function toggleDarkMode(){
+  if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+}
 </script>
