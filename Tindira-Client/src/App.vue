356cca130c74da0c2a1838f502fb627532278256
Ben-avrahami <ben.avrahami@intel.com>
2024-04-03 15:02:23 +0300

    Update App.vue

--- Tindira-Client/src/App.vue
+++ Tindira-Client/src/App.vue
@@ -9,10 +9,6 @@ import 'iconify-icon';
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-<<<<<<< HEAD
-=======
-      <HelloWorld msg="You did it!" />
->>>>>>> main
 
 
       <nav>
