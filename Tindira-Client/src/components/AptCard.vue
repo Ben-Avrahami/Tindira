02392ad26a682d61008532d81dd1c8c8c964fb8f
Ben-avrahami <ben.avrahami@intel.com>
2024-04-08 01:08:06 +0300

    add handle + drag side detection

--- Tindira-Client/src/components/AptCard.vue
+++ Tindira-Client/src/components/AptCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <VueDraggable ref="el" v-model="links" @end="onEnd">
+    <VueDraggable ref="el" v-model="links" @end="onEnd" handle=".drag-area">
         <Card>
             <template #header>
                 <Carousel :value="links" :numVisible="1" :numScroll="1" circular>
@@ -13,9 +13,13 @@
                     </template>
                 </Carousel>
             </template>
-            <template #title>Lior's Home</template>
+            <template #title>
+                <div class="drag-area">
+                    Lior's Home
+                </div>
+            </template>
             <template #subtitle>
-                <div class="flex justify-between items-center">
+                <div class="drag-area flex justify-between items-center">
                     Sublet- Tel Aviv
                     <Button severity="secondary" text rounded aria-label="Info" class="mr-2" style="font-size: 2rem;">
                         <template #icon>
@@ -25,13 +29,13 @@
                 </div>
             </template>
             <template #content>
-                <p class="m-0">
+                <p class="drag-area m-0">
                     A nice cosy home, in the most vibrant place in tel aviv
                 </p>
 
             </template>
             <template #footer>
-                <div class="mx-auto space-x-24 flex justify-center">
+                <div class="drag-area mx-auto space-x-24 flex justify-center drag-area">
 
                     <Button severity="secondary" rounded aria-label="Like">
                         <template #icon>
@@ -39,7 +43,7 @@
                         </template>
                     </Button>
 
-                    <Button class="text-rose-700" rounded aria-label="Like">
+                    <Button class="text-rose-700 drag-area" rounded aria-label="Like">
                         <template #icon>
                             <Icon icon="ph:heart"></Icon>
                         </template>
@@ -68,13 +72,23 @@ const links = ref([
     'https://www.thespruce.com/thmb/2_Q52GK3rayV1wnqm6vyBvgI3Ew=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/put-together-a-perfect-guest-room-1976987-hero-223e3e8f697e4b13b62ad4fe898d492d.jpg'
 ])
 
-function onEnd(event: any, offsetX: any, offsetY: any) {
+function onEnd(event: any) {
     console.log(event);
-    if (event.originalEvent.changedTouches[0].clientX > 0) {
-        console.log("right");
-    }
-    else {
-        console.log("left")
+    if (event.originalEvent instanceof TouchEvent) {
+        if (event.originalEvent.changedTouches[0].clientX > 0) {
+            console.log("right");
+        }
+        else {
+            console.log("left")
+        }
+    } else if (event.originalEvent instanceof MouseEvent) {
+        if (event.originalEvent.screenX > 1500) {
+            console.log("right");
+        }
+        if (event.originalEvent.screenX < 500){
+            console.log("left")
+        }
+        console.log("MouseEvent");
     }
 }
 
