30612b9b4c4e0837e5b1c62309d42fb46b05e621
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2024-04-11 19:26:34 +0300

    first login project finish

--- Tindira-Server/Lambda-Functions/Log-in/utils/auth.js
+++ Tindira-Server/Lambda-Functions/Log-in/utils/auth.js
@@ -10,7 +10,7 @@ function generateToken(userInfo) {
     expiresIn: "1h",
   });
 }
-
+``;
 function verifyToken(username, token) {
   return jwt.verify(token, process.env.JWT_SECRET, (error, response) => {
     if (error) {
