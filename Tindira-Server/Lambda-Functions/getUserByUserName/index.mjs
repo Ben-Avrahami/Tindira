7535b1174010db501dec7de97e0d56a6a77aea84
Omer Komissarchik <139342166+OmerK100@users.noreply.github.com>
2024-05-14 21:27:12 +0300

    Add files via upload

---
+++ Tindira-Server/Lambda-Functions/getUserByUserName/index.mjs
@@ -0,0 +1,162 @@
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import AWS from 'aws-sdk';
+
+const lambda = new AWS.Lambda();
+
+export const handler = async (event) => {
+
+  let queryParams = event.queryStringParameters;
+
+  if (!validateParams2(queryParams)) { // Check that query params for users have been passed
+    const response = {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify("Parameters haven't been set properly!")
+    };
+  
+    return response;
+  }
+
+  /*let body = JSON.parse(event.body)
+
+  if (!validateParams(queryParams, body)) { // Check that query params for users have been passed
+    const response = {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify("Parameters haven't been set properly!")
+    };
+  
+    return response;
+  }
+  
+  const lambdaParams = {
+    FunctionName: 'users-login-system', // Use this Lambda function to verify the token
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify({
+      httpMethod: "POST",
+      path: "/verify",
+      body: JSON.stringify({
+        token: body.token,
+        user: {
+          username: body.username
+        }
+      })
+    })
+  };
+
+  try {
+    const response = await lambda.invoke(lambdaParams).promise(); // Handle response from the "verify" Lambda function
+    const payloadMessage = JSON.parse(JSON.parse(response.Payload).body).message
+    if (!payloadMessage || payloadMessage !== "success") {
+      const res = {
+        statusCode: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify("Invalid verification token!")
+      };
+      return res;
+    }
+  } catch (error) { // Handle errors
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: "Internal server error"
+        })
+      };
+  }*/
+
+  const db = new DynamoDBClient({ region: 'us-east-2' }); // Connect to DynamoDB
+  
+  queryParams.username = "[" + queryParams.username + "]";
+  
+  let usernames = [];
+  
+  usernames = queryParams.username.slice(1, -1).split(','); // Convert query param to user ID array
+  
+  if (usernames[0] === "") {
+    usernames = [];
+  }
+  
+  let users = [];
+  
+  for (var i = 0; i < usernames.length; i++) { // Iterate over the IDs and query their corresponding user in the DB 
+    const params = {
+      TableName: "TindiraUsers",
+      Key: {
+        "username": { S: usernames[i] }
+      }
+    };
+    try {
+      const command = new GetItemCommand(params);
+      const res = await db.send(command);
+      if (res.Item !== undefined) {
+        users.push(res.Item);
+      } else {
+        users.push("User with provided ID does not exist!");
+      }
+    } catch (err) {
+      const response = {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify("Internal server error")
+      };
+  
+      return response;
+    }
+  }
+  
+  const response = { // Success, return the listings
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(users)
+  };
+  
+  return response;
+  
+};
+
+
+/*************************************************************************************************************************************************************************************/
+
+
+function validateParams(query, body) { // Validate query strings and request body
+  
+  if (query.username === undefined || query.username === "" || body === undefined) {
+    return false;
+  }
+
+  if (body.username === undefined || body.username === "" || body.token === undefined || body.token === "") {
+    return false;
+  }
+  
+  return true;
+  
+}
+
+function validateParams2(query) { // Validate query strings and request body
+  
+  if (query.username === undefined || query.username === "") {
+    return false;
+  }
+  
+  return true;
+  
+}
