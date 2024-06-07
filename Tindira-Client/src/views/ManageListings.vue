16f255d497f163ec5bdd84a446cc05a0ad8f53e8
Ben-avrahami <ben.avrahami@intel.com>
2024-06-07 16:13:45 +0300

    improve page loading

--- Tindira-Client/src/views/ManageListings.vue
+++ Tindira-Client/src/views/ManageListings.vue
@@ -3,20 +3,27 @@
         <h1 class="text-3xl font-bold text-center text-surface-300 pt-8 select-none mb-10">
             Manage Listings
         </h1>
-        <UsersListingList :listings="userListings"></UsersListingList>
+        <div>
+            <UsersListingList :listings="userListings"></UsersListingList>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 
 import { useAppStore } from '../stores/app'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import API from '@/api';
 import UsersListingList from '@/components/UsersListingList.vue';
 
 const userStore = useAppStore()
-let userListingsIds = ref((await API.getUsersByUserName([userStore.connectedUser!], ["listings"]))[0].listings);
-let userListings = ref((await API.getListingsById(userListingsIds.value)));
+let userListingsIds = ref();
+let userListings = ref();
+
+onMounted(async () => {
+    userListingsIds.value = (await API.getUsersByUserName([userStore.connectedUser!], ["listings"]))[0].listings;
+    userListings.value = await API.getListingsById(userListingsIds.value);
+})
 
 </script>
 
