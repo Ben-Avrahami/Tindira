89076774c17ed8b5cdf3ab5f69dd908935cc48cb
Ben-avrahami <ben.avrahami@intel.com>
2024-06-07 15:15:02 +0300

    Create ManageListings.vue

---
+++ Tindira-Client/src/views/ManageListings.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold text-center text-surface-300 pt-8 select-none mb-10">
+            Manage Listings
+        </h1>
+        <UsersListingList :listings="userListings"></UsersListingList>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { useAppStore } from '../stores/app'
+import { ref } from 'vue'
+import API from '@/api';
+import UsersListingList from '@/components/UsersListingList.vue';
+
+const userStore = useAppStore()
+let userListingsIds = ref((await API.getUsersByUserName([userStore.connectedUser!], ["listings"]))[0].listings);
+let userListings = ref((await API.getListingsById(userListingsIds.value)));
+
+</script>
+
+<style scoped></style>
