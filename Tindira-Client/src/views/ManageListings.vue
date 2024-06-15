6444ddfd0a18a4487c8495c47caa957ff91f7cbc
yonisimian <yonisimian@gmail.com>
2024-06-15 19:45:10 +0300

    show spinner when fetching data

--- Tindira-Client/src/views/ManageListings.vue
+++ Tindira-Client/src/views/ManageListings.vue
@@ -1,30 +1,34 @@
 <template>
+  <div>
+    <h1 class="text-3xl font-bold text-center text-surface-300 pt-8 select-none mb-10">
+      Manage Listings
+    </h1>
     <div>
-        <h1 class="text-3xl font-bold text-center text-surface-300 pt-8 select-none mb-10">
-            Manage Listings
-        </h1>
-        <div>
-            <UsersListingList :listings="userListings"></UsersListingList>
-        </div>
+      <UsersListingList :listings="userListings" />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
-import { useAppStore } from '../stores/app'
-import { onMounted, ref } from 'vue'
-import API from '@/api';
-import UsersListingList from '@/components/UsersListingList.vue';
+import { useAppStore } from '@/stores/app'
+import { onBeforeMount, ref } from 'vue'
+import API from '@/api'
+import UsersListingList from '@/components/UsersListingList.vue'
 
 const userStore = useAppStore()
-let userListingsIds = ref();
-let userListings = ref();
+const userListingsIds = ref()
+const userListings = ref()
 
-onMounted(async () => {
-    userListingsIds.value = (await API.getUsersByUserName([userStore.connectedUser!], ["listings"]))[0].listings;
-    userListings.value = await API.getListingsById(userListingsIds.value);
+const fetchListings = async () => {
+  userListingsIds.value = (
+    await API.getUsersByUserName([userStore.connectedUser!], ['listings'])
+  )[0].listings
+  userListings.value = await API.getListingsById(userListingsIds.value)
+}
+
+onBeforeMount(async () => {
+  await userStore.performAsyncAction(fetchListings)
 })
-
 </script>
 
 <style scoped></style>
